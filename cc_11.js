994669f326c0b47de0ba374aa3ab1e08e5094e8c
JTR254 <jackrindge@gmail.com>
2025-03-02 15:51:29 -0500

    Task 3 - Created Library Class

--- cc_11.js
+++ cc_11.js
@@ -1,6 +1,6 @@
 // Task 1 - Created Book Class
 
-class book { // creates book class
+class Book { // creates book class
     constructor(title, author, isbn, copies) {
         this.title = title;
         this.author = author;
@@ -16,14 +16,14 @@ class book { // creates book class
     }
 };
 
-const book1 = new book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5);
+const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5);
 console.log(book1.getDetails()); // Expected Output - Title: The Great Gaspy, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 5
 book1.updateCopies(-1);
 console.log(book1.getDetails()); // Expected Ouput - Title: The Great Gaspy, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4
 
 // Task 2 - Created Borrower Class
 
-class borrower { // creates a class that keep track of who borrows books
+class Borrower { // creates a class that keep track of who borrows books
     constructor(name, borrowerId) { 
         this.name = name;
         this.borrowerId = borrowerId;
@@ -39,8 +39,28 @@ class borrower { // creates a class that keep track of who borrows books
         }
     }
 }
-const borrower1 = new borrower("Alice Johnson", 201);
+const borrower1 = new Borrower("Alice Johnson", 201);
 borrower1.borrowBook("The Great Gatsby");
 console.log(borrower1.borrowedBooks); // logs the books that are currently being borrowed to the console
 borrower1.returnBook("The Great Gatsby");
 console.log(borrower1.borrowedBooks); // logs the books that are currently being borrowed to the console
+
+// Task 3 - Created Library Class
+
+class Library { // creates class that includes two arrays to keep track of books and borrowers
+    constructor () {
+        this.books = [];
+        this.borrowers = []
+    }
+    addBook(book) { // creates method that adds a book to the array
+        this.books.push(book); 
+    }
+    listBooks() { // creates method that adds books to the array
+        this.books.forEach(book => console.log(book.getDetails()));
+    }
+}
+
+const library = new Library();
+library.addBook(book1);
+library.listBooks();
+// Output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
